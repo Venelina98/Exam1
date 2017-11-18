3bbeecbf0ca1b74174e618020d8e9147701dc065
Venelina98 <venito_988@mail.bg>
2017-11-18 21:34:34 +0200

    I add my homework for week 8

---
+++ Exam1/CircleArea/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int radius = random.Next(33, 187);
+            Console.WriteLine("The circle random radius is {0}", radius);
+
+            double area = Math.Pow(radius, 2) * Math.PI;
+            Console.WriteLine("The circle area = {0:0.00}", area);
+
+        }
+    }
+}
